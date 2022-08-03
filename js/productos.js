fb1778be4d060ae17fb57315625c59e45bc8b6c4
rodriclav2809 <104847543+rodriclav2809@users.noreply.github.com>
2022-08-02 23:03:00 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,7 @@
+const productos = [
+    {id:01, tipo:"Remera", nombre:"Camiseta Golero 2022", precio: 1400, foto: "./media/camisetagolero2022.jpg" },
+    {id:02, tipo:"Remera", nombre:"Camiseta Local 2021", precio: 1400, foto: "./media/CAMISETALOCAL2021.png" },
+    {id:03, tipo:"Remera", nombre:"Camiseta Nacional Dama 2021", precio: 1000, foto: "./media/camisetaNacionaldama2021.png" },
+    {id:04, tipo:"Remera", nombre:"Camiseta Nacional Uruguay 2022", precio: 1400, foto: "./media/camisetaNacionalUruguay2022.jpg" },
+    {id:06, tipo:"Remera", nombre:"Camiseta Lucho 2022", precio: 1500, foto: "./media/camisetalucho2022.jpg" },
+  ];
